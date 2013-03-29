8d286257969f72af32a42f635e53b2ad5e4b2f27
Drew Hunter <dhunter@inviqa.com>
2013-03-29 19:39:40 +0000

    Add new config tests

--- tests/unit/app/code/community/Dh/ShipNote/ConfigTest.php
+++ tests/unit/app/code/community/Dh/ShipNote/ConfigTest.php
@@ -15,6 +15,16 @@ class Dh_ShipNote_ConfigTest extends PHPUnit_Framework_TestCase
     }
 
     /**
+     * Ensure that the module has been installed and registered correctly
+     */
+    public function test_that_module_is_in_community_code_pool()
+    {
+        $codePool = Mage::app()->getConfig()
+            ->getModuleConfig(MODULE_NAME)->codePool;
+        $this->assertEquals($codePool, 'community');
+    }
+
+    /**
      * Ensure that the css file is present
      */
     public function test_that_the_module_css_file_exists_in_correct_theme_and_package()
@@ -24,5 +34,14 @@ class Dh_ShipNote_ConfigTest extends PHPUnit_Framework_TestCase
         $cssFile = $skinDir . '/css/shipnote.css';
 
         $this->assertTrue(file_exists($cssFile));
+    }
+
+    /**
+     * Ensure that the db setup has ran
+     */
+    public function test_that_module_db_setup_has_ran()
+    {
+        $setupVersion = Mage::getResourceSingleton('core/resource')->getDbVersion('shipnote_setup');
+        $this->assertTrue(is_string($setupVersion));
     }
 }
